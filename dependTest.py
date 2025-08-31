1f4617fa22e540b1e38bfe2c43a5dba7f515ab55
DJ-DT\user <djimme@gmail.com>
2025-08-31 19:38:02 +0900

    0831 의존성 예제

---
+++ dependTest.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI, Depends, Query
+
+app = FastAPI()
+
+# 의존성 함수
+def user_dep(name:str = Query(...), gender:str=Query(...)):
+    return {"name":name, "valid":True}
+
+# 경로함수/ 웹 엔드포인트:
+@app.get('/user')
+def get_user(user:dict=Depends(user_dep)) -> dict:
+    return user
+
+# 의존성 함수
+def check_dep(name:str = Query(...), gender:str=Query(...)):
+    if not name:
+        raise
+
+# 경로함수/ 웹 엔드포인트
+@app.get('/check_user', dependencies=[Depends(check_dep)])
+def check_user() -> bool:
+    return True
