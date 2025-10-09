4e626611a277a1c34c92ff8ea295a8f56d912f99
Dijay Lee <djimme@gmail.com>
2025-10-10 00:42:18 +0900

    1009 더블테스트를 위해 web creature, fake creature 변경

--- src/web/creature.py
+++ src/web/creature.py
@@ -1,7 +1,14 @@
-from fastapi import APIRouter
+import os
+
+from fastapi import APIRouter, HTTPException
 from model.creature import Creature
-# import fake.creature as service
-import service.creature as service
+
+if os.getenv("CRYPTID_UNIT_TEST"):
+    from fake import creature as service
+else:   
+    from service import creature as service
+
+from error import Missing, Duplicate
 
 router = APIRouter(prefix="/creature")
 
@@ -12,22 +19,38 @@ def get_all() -> list[Creature]:
 
 @router.get("/{name}")
 def get_one(name) -> Creature | None:
-    return service.get_one(name)
+    try: 
+        return service.get_one(name)
+    except Missing as e:
+        raise HTTPException(status_code=404, detail=e.msg)
 
-# 나머지 엔드포인트. 현재는 아무일도 하지 않는다. 
 @router.post("")
-@router.post("/")
+@router.post("/", status_code=201)
 def create(creature:Creature) -> Creature:
-    return service.create(creature)
+    try:
+        return service.create(creature)
+    except Duplicate as e:
+        raise HTTPException(status_code=409, detail=e.msg) from e
 
 @router.patch("/{name}")
 def modify(name:str, creature:Creature) -> Creature:
-    return service.modify(name, creature)
+    try:
+        return service.modify(name, creature)
+    except Missing as e:
+        raise HTTPException(status_code=404, detail=e.msg) from e
 
 @router.put("/{name}")
 def replace(name:str, creature:Creature) -> Creature:
-    return service.replace(name, creature)
+    try:
+        return service.replace(name, creature)
+    except Missing as e:
+        raise HTTPException(status_code=404, detail=e.msg) from e
+    
 
 @router.delete("/{name}")
 def delete(name:str) -> bool:
-    return service.delete(name)
+    try:
+        return service.delete(name)
+    except Missing as e:
+        raise HTTPException(status_code=404, detail=e.msg) from e
+    
